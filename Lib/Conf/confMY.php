0038b0a346ee349540756469c767de701d35be49
Danielmosquera813 <ldmosquera83@misena.edu.co>
2020-09-07 19:06:03 -0500

    Organización del correo

--- Lib/Conf/confMY.php
+++ Lib/Conf/confMY.php
@@ -3,7 +3,7 @@
     $server="127.0.0.1";//es como la dirreccion y ip del localost, como por defecto
     $user="root";
     $pass=""; //contraseña de la base de datos de postgres
-    $database="vial_manager2_6";//Colocar el nombre respectivo de la BD
+    $database="vialmanager";//Colocar el nombre respectivo de la BD
     //PD: juan sebastian no recuerda la version de la base de datos de mysql XD
 
     $port="3306";//5432

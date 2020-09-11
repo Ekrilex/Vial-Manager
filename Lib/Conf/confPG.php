2aa052fb8c6d7f584f9ef7fb2e514ecf485cdf2f
Kevin Collazos <kwcollazos@misena.edu.co>
2020-09-10 20:33:18 -0500

    Restauracion de configuracion en conexion con postgres

--- Lib/Conf/confPG.php
+++ Lib/Conf/confPG.php
@@ -1,9 +1,9 @@
 <?php
 
     $host = 'localhost';
-    $port = '5433';
+    $port = '5432';
     $database = 'VialManager3_1';
     $user = 'postgres';
-    $pass = '12345'; //la contraseña de la base de datos es el mismo nombre del user
+    $pass = 'postgres'; //la contraseña de la base de datos es el mismo nombre del user
 
 ?>

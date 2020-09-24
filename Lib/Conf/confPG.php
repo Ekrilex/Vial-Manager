428cfff8bec559e7fdbe90ccf3a8e7be86c4ffe5
Danielmosquera813 <ldmosquera83@misena.edu.co>
2020-09-23 23:13:07 -0500

    Solucion de error en una validación de deterioro

--- Lib/Conf/confPG.php
+++ Lib/Conf/confPG.php
@@ -2,7 +2,7 @@
 
     $host = 'localhost';
     $port = '5432';
-    $database = 'VialManager3_1';
+    $database = 'VialManager3_3';
     $user = 'postgres';
     $pass = 'postgres'; //la contraseña de la base de datos es el mismo nombre del user
 
